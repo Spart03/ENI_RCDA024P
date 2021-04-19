c66041a1946fec0922bc4b2ff50961dc91e6c3c4
Alex Mth <alexmathieu@msn.com>
2021-04-19 17:02:13 +0200

    init projet

---
+++ TP1_module2/Models/Carre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1_module2.Models
+{
+    public class Carre : Rectangle
+    {
+        public override int Largeur => Longueur;
+
+        public override string ToString()
+        {
+            return $"Carré de coté={Longueur}" + Environment.NewLine + base.ToString();
+        }
+    }
+}
---
+++ TP1_module2/Models/Forme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1_module2.Models
+{
+    public abstract class Forme
+    {
+        public abstract double Aire { get; }
+        public abstract double Perimetre { get; }
+
+        public override string ToString()
+        {
+            return $"Aire = {Aire}" + Environment.NewLine + $"Perimetre = {Perimetre}" + Environment.NewLine;
+        }
+    }
+}
